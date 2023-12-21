21aa587a8cffb9cc5c7f625ed2ff09e445e07c38
Ekaterina <komsa_9191@mail.ru>
2023-12-21 15:01:17 +1000

    feat: add solution tasks: get-email-domain and mac-address

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  const lastIndexSymbol = email.lastIndexOf('@') + 1;
+  const domain = email.substring(lastIndexSymbol, email.length);
+
+  return domain;
 }
 
 module.exports = {
