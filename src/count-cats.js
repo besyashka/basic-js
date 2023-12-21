67244af54660e99c5dfcda6ce4baa62f8e6dbde1
Ekaterina <komsa_9191@mail.ru>
2023-12-21 11:11:41 +1000

    feat: add solution tasks: count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let cat = 0;
+
+  matrix.forEach(item => {
+    item.forEach(element => {
+      if (element === '^^') {
+        cat++;
+      }
+    })
+  })
+  return cat;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let result = '';
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  members.forEach(element => {
+    if (typeof element === 'string') {
+      const firstLetter = element.trim()[0].toUpperCase();
+      result += firstLetter;
+    }
+  });
+  const sortResult = result.split('').sort((a, b) => a.localeCompare(b))
+  return sortResult.join('');
 }
 
 module.exports = {
